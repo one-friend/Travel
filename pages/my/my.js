ce4bc8cd35760341c38e376cdf39e5102751af82
hutianqi <tianqihu@sohu-inc.com>
2025-11-13 17:20:54 +0800

    update

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,9 +1,10 @@
-const app = getApp()
+const app = getApp();
+
 Page({
   data: {
     userInfo: {
       avatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0',
-      nickName:'微信用户'
+      nickName: '微信用户'
     },
     visitedCities: [],
     maps: [
@@ -11,52 +12,78 @@ Page({
       { name: '足迹2', id: 'map2' },
       { name: '足迹3', id: 'map3' }
     ],
+    currentMapIndex: 0,
+    showDetailModal: false,
+    currentCity: {}
   },
 
   onLoad() {
-    // 假设你已经获取了用户信息和已点亮的城市
-    const visitedCities = wx.getStorageSync('visitedCities') || {}
+    const userInfo = wx.getStorageSync('userInfo') || this.data.userInfo;
+    const visitedCities = wx.getStorageSync('visitedCities') || {};
     this.setData({
-      visitedCities: this.getCitiesWithTrueValue(visitedCities), // 从缓存获取已点亮城市
+      userInfo,
+      visitedCities: this._parseVisitedCities(visitedCities)
     });
   },
-  getCitiesWithTrueValue(data) {
+
+  // 提取已点亮城市
+  _parseVisitedCities(data) {
     return Object.keys(data)
-      .filter(key => data[key]?.on === true)  // 过滤出值为 true 的城市
-      .map(key => key.split('-')[1]);     // 提取城市名（按 "-" 分割，取第二部分）
+      .filter(key => data[key]?.on === true)
+      .map(key => ({
+        key,
+        name: key.split('-')[1],
+        date: data[key].date,
+        datetime: data[key].datetime,
+        note: data[key].note || ''
+      }))
+      .sort((a, b) => (b.datetime || '').localeCompare(a.datetime || ''));
   },
+
   switchMap(e) {
     const index = e.currentTarget.dataset.index;
     this.setData({ currentMapIndex: index });
   },
-  chooseAvatar(e){
+
+  chooseAvatar(e) {
     let { userInfo } = this.data;
     userInfo.avatarUrl = e.detail.avatarUrl;
-    this.asyncUserInfo(userInfo);
+    this._syncUserInfo(userInfo);
   },
 
-  inputNickName(e){
+  inputNickName(e) {
     let { userInfo } = this.data;
     userInfo.nickName = e.detail.value;
-    this.asyncUserInfo(userInfo);
+    this._syncUserInfo(userInfo);
   },
 
-  asyncUserInfo(userInfo){
-    this.setData({
-      userInfo
-    })
-    wx.setStorage({
-      key:'userInfo',
-      data: userInfo
-    })
-    app.setGlobalData('userInfo',userInfo);
+  _syncUserInfo(userInfo) {
+    this.setData({ userInfo });
+    wx.setStorageSync('userInfo', userInfo);
+    app.setGlobalData && app.setGlobalData('userInfo', userInfo);
   },
+
+  // 显示城市详情
+  showCityDetail(e) {
+    const city = e.currentTarget.dataset.city;
+    this.setData({
+      showDetailModal: true,
+      currentCity: city
+    });
+  },
+
+  // 关闭弹层
+  closePopup() {
+    this.setData({
+      showDetailModal: false,
+      currentCity: {}
+    });
+  },
+
   // 退出登录
   logout() {
-    wx.removeStorageSync('userInfo');  // 清除用户信息缓存
-    wx.removeStorageSync('visitedCities');  // 清除已点亮城市缓存
-    wx.reLaunch({
-      url: '/pages/login/login',  // 跳转到登录页
-    });
+    wx.removeStorageSync('userInfo');
+    wx.removeStorageSync('visitedCities');
+    wx.reLaunch({ url: '/pages/login/login' });
   }
 });

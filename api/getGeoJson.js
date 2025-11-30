16d42c3006871bcbd13190e22b6cab6685800292
hutianqi <hutianqipg@163.com>
2025-11-30 22:40:58 +0800

    api

--- api/getGeoJson.js
+++ api/getGeoJson.js
@@ -1,9 +1,16 @@
 export const geojson = (provinceCode) => {
   return new Promise((resolve,reject) => {
+    console.log(provinceCode)
+    const geo =  wx.getStorageSync(provinceCode);
+    if(geo){
+      resolve(geo)
+      return
+    }
     wx.request({
       url: `https://geojson.cn/api/china/1.6.2/${provinceCode}.json`,
       success: function(res) {
         resolve(res && res.data)
+        wx.setStorageSync(provinceCode, res.data)
       }
     })
   })
